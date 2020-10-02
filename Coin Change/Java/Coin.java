5e77267b58caf894771dc918306e72c27c37646d
sanfernoronha <sanfernoronha99@gmail.com>
2020-10-02 14:17:28 +0530

    Added coin change

---
+++ Coin Change/Java/Coin.java
@@ -0,0 +1,30 @@
+class Coin {
+    static final int INF = 100000;
+
+    public static int  min(int x,int y) {
+        if(x<y)
+        return x;
+        return y;
+    }
+
+    public static int coinChange(int d[],int n,int k) {
+        int[] M = new int[n+1];
+        M[0] = 0;
+
+        for(int j=1;j<=n;j++){
+            int minimum = INF;
+            for(int i=1;i<=k;i++){
+                if(j >=d[i]){
+                    minimum = min(minimum,1+M[j-d[i]]);
+                }
+            }
+            M[j] = minimum;
+        }
+        return M[n];
+    }
+
+    public static void main(String[] args) {
+        String[] in = 
+        System.out.println(coinChange(d,5,3));
+    }
+}
